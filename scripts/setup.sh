8780949c4698000c6124e6a5d192237efe3b11a5
Shridal <111167066+Sridal@users.noreply.github.com>
2022-11-08 21:36:13 +0545

    First

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,6 @@ sudo -H pip install psutil
 cp -r config/alacritty/ ~/.config/
 cp -r config/picom/ ~/.config/
 cp -r config/qtile/ ~/.config/
-cp -r config/picom/ ~/.config/
+cp -r config/neofetch/ ~/.config/
+cp -r config/rofi/ ~/.config/
+cp -r config/networkmanager-dmenu/ ~/.config/
